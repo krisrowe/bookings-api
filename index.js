12351119115fb4d4c5a09bfcd9aabe05c39580e1
Kris Rowe <kris@rowelink.com>
2024-03-02 21:38:27 -0600

    Avoid failure on PUT

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const moment = require('moment'); // Make sure to install moment
 const app = express();
 const port = process.env.PORT || 8080;
+const fieldMap = require('./field-map.json');
 
 // Check if API_KEY is set, if not exit with status code 1
 if (!process.env.API_KEY) {
@@ -38,7 +39,7 @@ async function invokeBackendService({ method, url, data }) {
   const idToken = await client.idTokenProvider.fetchIdToken(url);
   console.log('Token retrieved');
 
-  console.log('Invoking backend service');
+  console.log('Invoking backend service', method, url, data);
   const response = await axios({
     method,
     url,
@@ -83,16 +84,47 @@ async function fetchDataAndRespond(req, res) {
   }
 }
 
-// New convertInput function
 function convertInput(input) {
   let converted = {};
-  if (input.hasOwnProperty('Door Access')) {
-    if (input['Door Access']) {
-      converted.doorAccess = moment(input['Door Access'], 'MM/DD/YYYY HH:mm:ss A').format('YYYY-MM-DD');
-    } else {
-      converted.doorAccess = '';
+
+  Object.keys(input).forEach(key => {
+    // Find the corresponding field in the field map based on the display name
+    const fieldInfo = Object.entries(fieldMap).find(([_, {display}]) => display === key);
+    if (!fieldInfo) return; // Skip if the field is not in the field map
+
+    const [fieldName, {type}] = fieldInfo;
+
+    switch (type) {
+      case 'date':
+        if (input[key]) {
+          converted[fieldName] = moment(input[key], 'MM/DD/YYYY HH:mm:ss A').format('YYYY-MM-DD');
+        }
+        break;
+      case 'number':
+        if (input[key]) {
+          converted[fieldName] = parseFloat(input[key]);
+        }
+        break;
+      case 'boolean':
+        let value = input[key];
+        // Convert various representations of boolean to true boolean values
+        if (typeof value === 'string') {
+          value = value.toLowerCase();
+          converted[fieldName] = value === 'true' || value === 'yes' || value === '1';
+        } else if (typeof value === 'number') {
+          converted[fieldName] = value !== 0;
+        } else {
+          // Assume it's already a proper boolean if not a string or number
+          converted[fieldName] = Boolean(value);
+        }
+        break;
+      case 'string':
+      default: // Default case to handle any other types not explicitly processed
+        converted[fieldName] = input[key];
     }
-  }
+    // Note: 'datetime' types are intentionally omitted to be handled by the server
+  });
+
   return converted;
 }
 
@@ -104,26 +136,22 @@ app.put('/o/:orgId/reservations/:conf', async (req, res) => {
   const { orgId, conf } = req.params;
   const convertedData = convertInput(req.body);
 
-  if (convertedData.hasOwnProperty('doorAccess')) {
-    const serviceHost = process.env.BOOKINGS_SERVICE_HOST;
-    const targetAudience = `https://${serviceHost}`;
-    const apiUrl = `${targetAudience}/events`;
-    
-    const postData = {
-      type: "update",
-      conf: conf,
-      ...convertedData // Spread the converted data into postData
-    };
+  const serviceHost = process.env.BOOKINGS_SERVICE_HOST;
+  const targetAudience = `https://${serviceHost}`;
+  const apiUrl = `${targetAudience}/events`;
+  
+  const postData = {
+    type: "update",
+    conf: conf,
+    ...convertedData // Spread the converted data into postData
+  };
 
-    try {
-      await invokeBackendService({ method: 'POST', url: apiUrl, data: postData });
-      res.send('Door access updated successfully');
-    } catch (error) {
-      console.error('Error updating door access:', error);
-      res.status(500).send('Error updating door access');
-    }
-  } else {
-    res.status(400).send('Invalid request data: "Door Access" field is missing or incorrect.');
+  try {
+    await invokeBackendService({ method: 'POST', url: apiUrl, data: postData });
+    res.send('Door access updated successfully');
+  } catch (error) {
+    console.error('Error updating door access:', error);
+    res.status(500).send('Error updating door access');
   }
 });
 
